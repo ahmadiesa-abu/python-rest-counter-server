4c826979ca3e25e97482ccb2d49ed3a31cf9cd69
Ahmad Musa <53178237+ahmadiesa-abu@users.noreply.github.com>
2019-10-08 12:26:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,16 @@ api = Api(app)
 count = 0
 
 class Counter(Resource):
+    def get(self):
+        global count
+        count = count+1
+        return count, 200
+    
     def post(self):
+        global count
         count = count+1
         return count, 200
         
 api.add_resource(Counter, "/counter")
 
-app.run(debug=True)
+app.run(host='0.0.0.0',debug=True)
